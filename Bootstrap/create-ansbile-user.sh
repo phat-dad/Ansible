939528c55b2bc0f8cb7872169ce0475079cd94b6
phat-dad <161524147+phat-dad@users.noreply.github.com>
2024-02-29 10:29:29 -0500

    Update create-ansbile-user.sh

--- Bootstrap/create-ansbile-user.sh
+++ Bootstrap/create-ansbile-user.sh
@@ -2,11 +2,10 @@
 
 ## SSH Public Key
 ## Replace this public key with your own public key
-pubkey='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzPZ1Y5pp0aheWU+fV4Fgy2wdtRBi70+PMJWWmZgy1sf3vsBqZWznX+4/wkqjCyppYK/35eHUEnHLfXahnbxKGebZT2S/kuiLcdzlw3tCNJUT++bQ9QMk3hnhTHdb4l3DINaUXyiJj11thcsV0Hz6NiLAKqbjweCIXjJNim4zvCxfTihE4xX0ciGTZ2gWvwxFzFupwPQ1I0QHZ7iBdFC+8Ii+D1ffgpM8Rfx/VgMVRz8sc3CnvsozPYU1DR2tJMGY3WlEgsDq2HTQw+wkyYQNdj+R4Tayy6C9AzNxs5Z4cfBvjU4bRLGx2WgzmFQyx4K7xR4W3wzGaQBx8I7TnA5TqkDR9ROoVWb5gJ0awlfl5K8Sf15BojnS3PZfcn4sLMakb5eGRAKh03Ge6WrKEvwusG47fOE6Qfv2FeugNMTpgBtiCERC3aIsKMXpmtUJ2A1aKEZCZIQUsHUaZGfo27e1fwVxqAOL5mFivT7Z+Wa6oJtfPHZadrsLVmjo1ClL5Skz0+Q9PJED387Ni/6SHTJqVsmgcmPCvdCr5+VGaHabiYdMJOuam+vSBLteIRy5DComqB3Nw96SBovuaD9WEm4HKARxPyWk9ck3giJctQIzFyZRXvw0qJORjUlAfPVIVTBR/+HZWqTj8RodSIpMOgj5mJfS0W3kx65Sk5APtnMDgvw=='
-
+pubkey=''
 ## SSH Username for Ansible
 ## Replace this username with the user you'd like to create
-ansibleuser='anssvc'
+ansibleuser=''
 
 echo "This script must be run as root"
 
